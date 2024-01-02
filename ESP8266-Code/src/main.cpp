7d00a7d797c784fe0f5a9f5ff1e034bd15a95f6b
binsim <jonasspitzhirn@gmail.com>
2024-01-02 15:48:34 +0100

    Read poti values

--- ESP8266-Code/src/main.cpp
+++ ESP8266-Code/src/main.cpp
@@ -1,8 +1,30 @@
 #include <Arduino.h>
+
+#define POTI_PIN A0
+
 void setup()
 {
+	Serial.begin(9600);
 }
 
 void loop()
 {
+	// ############################################ READING POTI ######################################################
+	{
+		// TODO: Smooth out values
+
+		// Saving previously read values
+		static int prevPotiValue = 0;
+		// Read value from Poti
+		int potiValue = analogRead(POTI_PIN);
+		if (potiValue != prevPotiValue)
+		{
+			Serial.print("Poti value changed: ");
+			Serial.println(potiValue);
+
+			// TODO: Send value to ESP32
+
+			prevPotiValue = potiValue;
+		}
+	}
 }

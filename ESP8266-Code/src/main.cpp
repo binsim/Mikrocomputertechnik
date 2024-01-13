08f9a53179d8332d25ded071a434fca35426df5e
binsim <jonasspitzhirn@gmail.com>
2024-01-13 18:16:02 +0100

    esp8266 EspNow

--- ESP8266-Code/src/main.cpp
+++ ESP8266-Code/src/main.cpp
@@ -1,10 +1,14 @@
 #include <Arduino.h>
+#include <espnow.h>
+#include <ESP8266WiFi.h>
 
 #define POTI_PIN A0
 #define RELAY_PIN 0
 
 // Region FunctionDeclaration
 void setRelay(bool on);
+void sendPotiValue(const int value);
+void onEspNowCallback(u8 *mac_addr, u8 *incomingData, u8 len);
 // EndRegion FunctionDeclaration
 
 void setup()
@@ -14,6 +18,14 @@ void setup()
 	// Setup Relay to be output and off
 	pinMode(RELAY_PIN, OUTPUT);
 	setRelay(false);
+
+	// Set WiFi mode to station mode (connecting to an access point)
+	WiFi.mode(WIFI_STA);
+	Serial.print("MAC-Address: ");
+	Serial.println(WiFi.macAddress());
+
+	// Set callback function for received messages
+	esp_now_register_recv_cb(onEspNowCallback);
 }
 
 void loop()
@@ -31,7 +43,8 @@ void loop()
 			Serial.print("Poti value changed: ");
 			Serial.println(potiValue);
 
-			// TODO: Send value to ESP32
+			// Send value to ESP32
+			sendPotiValue(potiValue);
 
 			prevPotiValue = potiValue;
 		}
@@ -41,4 +54,18 @@ void loop()
 void setRelay(bool on)
 {
 	digitalWrite(RELAY_PIN, on ? HIGH : LOW);
+}
+
+void sendPotiValue(int value)
+{
+	u8 mac[6] = {};
+	u8 data[] = {value >> 8 & 0xFF, value & 0xFF};
+
+	// Send data to Esp32 device
+	esp_now_send(mac, data, 2);
+}
+
+void onEspNowCallback(u8 *mac_addr, u8 *incomingData, u8 len)
+{
+	// TODO: Handle
 }

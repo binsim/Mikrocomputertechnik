c0aeec16e8ef1b84d94f90102bf42d9849184520
binsim <jonasspitzhirn@gmail.com>
2024-01-17 21:21:11 +0100

    Print MAC-Address

--- ESP32-Code/src/main.cpp
+++ ESP32-Code/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <esp_now.h>
+
+void onEspNowCallback(const uint8_t *mac_addr, const uint8_t *incomingData, int len);
 
 void setup()
 {
+	Serial.print("MAC-Address: ");
+	Serial.println(WiFi.macAddress());
+
+	esp_now_register_recv_cb(onEspNowCallback);
 }
 
 void loop()
+{
+}
+
+void onEspNowCallback(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
 {
 }

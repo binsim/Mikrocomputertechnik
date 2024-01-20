ca3c2c50e70aaf852f8e94188865467ba7c99b91
floergri <148945984+floergri@users.noreply.github.com>
2024-01-20 16:17:04 +0100

    main.cpp

--- ESP32-Code/src/main.cpp
+++ ESP32-Code/src/main.cpp
@@ -1,7 +1,24 @@
 #include <Arduino.h>
 
+#include "FlashUtil.h"
+
+FlashUtil flashUtil;
+
 void setup()
 {
+    Serial.begin(115200); //open serial port
+    delay(100); //wait for PC to open serial port
+
+    Serial.println("###################################  STARTUP ################################");
+
+    if(!flashUtil.getConfigured())
+    {
+        Serial.println("No pin has been configured yet. Please configure a pin first.");
+    }
+    
+    //TODO: add code here
+
+    Serial.println("###################################  Setup Done!!! ################################");
 }
 
 void loop()

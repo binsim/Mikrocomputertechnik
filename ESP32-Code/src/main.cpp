21ec427960e58bd54c4f912d2fc0a48332100408
floergri <148945984+floergri@users.noreply.github.com>
2024-01-21 18:18:00 +0100

    cleared setup

--- ESP32-Code/src/main.cpp
+++ ESP32-Code/src/main.cpp
@@ -6,15 +6,6 @@ FlashUtil flashUtil;
 
 void setup()
 {
-    Serial.begin(115200); //open serial port
-    delay(100); //wait for PC to open serial port
-
-    Serial.println("###################################  STARTUP ################################");
-
-    
-    //TODO: add code here
-
-    Serial.println("###################################  Setup Done!!! ################################");
 }
 
 void loop()
